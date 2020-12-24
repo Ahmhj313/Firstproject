f4bc3d35e69fddf0a8b8757328674e90839abb6b
Adnan Jasem <76551333+Ahmhj313@users.noreply.github.com>
2020-12-24 10:57:13 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+print("")
+print("This is an x/y function program")
+print("Insert your x value")
+x = input()
+print("Now insert your y value")
+y = input()
+x = int(x)
+y = int(y)
+z = x/y
+print("Your result is = " + str(z))
