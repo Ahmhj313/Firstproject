b87a225993e353ea4925816b1dfb68d80b4d9cd2
Adnan Jasem <76551333+Ahmhj313@users.noreply.github.com>
2020-12-24 10:57:46 +0300

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,11 +1,15 @@
 print("Hello world!")
 print("")
 print("This is an x/y function program")
+
 print("Insert your x value")
 x = input()
+
 print("Now insert your y value")
 y = input()
+
 x = int(x)
 y = int(y)
+
 z = x/y
 print("Your result is = " + str(z))
