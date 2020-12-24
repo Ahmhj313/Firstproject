4672f5d2440b5c881c96bb7401fe24616be6dd8a
Adnan Jasem <76551333+Ahmhj313@users.noreply.github.com>
2020-12-24 11:49:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+#Informs you that you need to type the password
 print("Enter the password please")
 x = input()
+#If the password is: I like python
 if (x == "I like python"):
+#It will say: Access Granted
    print("Access Granted!")
+#But if it was something else, it will say: Acceess denied
 else:
    print("Access Denied")
